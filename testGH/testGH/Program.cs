3ab96e093d12274f3322fc223b988599bdf53428
JollyBravo <112023264+JollyBravo@users.noreply.github.com>
2022-10-29 19:22:04 +0300

    Update Program.cs

--- testGH/testGH/Program.cs
+++ testGH/testGH/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("LOL");
             Console.WriteLine("PPZ");
+            Console.WriteLine("ddd");
         }
     }
 }
